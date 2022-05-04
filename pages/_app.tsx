87e97f45c1340e4c577b8a0b063f0375fdfa4881
Pawan Paudel <pawanpaudel93@gmail.com>
2022-05-04 22:44:03 +0545

    initial dapp setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL!
+  const appId = process.env.NEXT_PUBLIC_APP_ID!
+
+  return (
+    <MoralisProvider serverUrl={serverUrl} appId={appId}>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
