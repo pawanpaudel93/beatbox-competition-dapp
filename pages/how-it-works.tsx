0833d58cb488b20a5cf3d7d9ac174c485dcfa1c4
Pawan Paudel <pawanpaudel93@gmail.com>
2022-05-28 07:43:55 +0000

    adding how it works

--- pages/how-it-works.tsx
+++ pages/how-it-works.tsx
@@ -1,7 +1,38 @@
+import { Center, Heading, ListItem, OrderedList, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
 const WorkingConcept: NextPage = () => {
-  return <></>
+  const texts = [
+    'A competition creator/organizer creates a competition and starts the wildcard submission and also adds judges to judge the competition',
+    'The competition wildcard submission is ended by clicking the end wildcard submission button in the settings. And now its the work of the judges to select top sixteen wildacards each.',
+    'Creator then finalizes the 16 beatbox wildcards and add it to the contract and contract calculates the 8 random battles for the Top 16 round.',
+    'Now the admin can start the battle funding the required amount for the battle and judges can vote till the battle time period which can be set by the organisor/creator.',
+    'The winner is then awarded the prize money when the battle is overby the contract and creator can start the next battle.',
+    'This continues till the state of the competition changes from Top 16 to Top 8, Top 8 to Semifinal, Semifinal to final and the competition is completed after the final winner is selected.',
+  ]
+  return (
+    <>
+      <Heading
+        fontWeight={600}
+        fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+        lineHeight={'110%'}
+        textAlign={'center'}
+      >
+        How it{' '}
+        <Text as={'span'} color={'blue.400'}>
+          works?
+        </Text>
+      </Heading>
+
+      <Center>
+        <OrderedList spacing={4} fontSize="xl" pt="6">
+          {texts.map((text, index) => (
+            <ListItem key={index}>{text}</ListItem>
+          ))}
+        </OrderedList>
+      </Center>
+    </>
+  )
 }
 
 export default WorkingConcept
